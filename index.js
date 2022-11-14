4db68c8fa716b651f39801cc369f64e94317e157
Hlorenzoo <hernanlorenzo97@hotmail.com>
2022-11-13 21:10:25 -0300

    Home dinamico, con cards mapeadas en base a la respuesta de la api

--- index.js
+++ index.js
@@ -10,3 +10,47 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
 });
+
+const mostrarData = (data) => {
+  let body = "";
+  for (var i = 0; i < data.data.length; i++) {
+    console.log(data.data);
+    body += `<div class="card">
+    <div class="card-image">
+      <img
+        src="${data.data[i].url_image}"
+        alt="..."
+      />
+      <i class="bx bx-star"></i>
+    </div>
+    <div class="card-content">
+      <h3>${data.data[i].name}</h3>
+      <p>Descripcion del producto</p>
+      <h3>$${data.data[i].price}</h3>
+      <button class="add-to-cart">Comprar</button>
+    </div>
+  </div>`;
+  }
+  document.getElementById("data").innerHTML = body;
+  console.log(body);
+};
+
+//Obtener productos fetch inicial
+
+let product = {};
+const getResponse = async () => {
+  let url = "http://localhost:8080/api/products/";
+
+  // const response = await fetch(url, {
+  //   method: "GET",
+  // });
+  // if (!response.ok) {
+  //   throw new Error(`HTTP error! Estado : ${response.status}`);
+  // }
+  // product = await response.json();
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => mostrarData(data))
+    .catch((error) => console.log(error));
+};
+getResponse();
